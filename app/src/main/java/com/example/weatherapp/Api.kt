73b048d73d7713163e8d154f3120018d6b3828c4
Mitch Y <mitchellsimy@gmail.com>
2022-02-15 21:47:21 -0600

    Part 1 of assignment
    Part 1 of assignment

---
+++ app/src/main/java/com/example/weatherapp/Api.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    // This is making a GET request.
+    @GET("weather")
+    fun getCurrentConditions(
+        @Query("zip") zip: String = "55127",
+        @Query("units") units: String = "imperial",
+        @Query("appid") appId: String = "127b4de6be5116abbdbaa8296a35f7e1",
+
+        ) : Call<CurrentConditions>
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,17 +5,75 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val apiKey = "127b4de6be5116abbdbaa8296a35f7e1"
+
+    private lateinit var api: Api
+    private lateinit var cityName: TextView
+    private lateinit var currentTemp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastBtn = findViewById<Button>(R.id.forecastButton);
+        cityName = findViewById(R.id.cityName)
+        currentTemp = findViewById(R.id.temperature)
+
+        val forecastBtn = findViewById<Button>(R.id.forecastButton)
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        api = retrofit.create(Api::class.java)
 
         forecastBtn.setOnClickListener(View.OnClickListener {
             val forecastIntent = Intent(this, ForecastActivity::class.java)
-            startActivity(forecastIntent);
+            startActivity(forecastIntent)
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val call: Call<CurrentConditions> = api.getCurrentConditions("55127")
+        call.enqueue(object: Callback<CurrentConditions> {
+            override fun onResponse(
+                call: Call<CurrentConditions>,
+                response: Response<CurrentConditions>
+            ) {
+                val currentConditions = response.body()
+
+                currentConditions?.let {
+                    bindData(it)
+                }
+            }
+
+            override fun onFailure(call: Call<CurrentConditions>, t: Throwable) {
+
+            }
+
+
+        })
+    }
+
+    private fun bindData(currentConditions: CurrentConditions) {
+        cityName.text = currentConditions.name
+//        currentTemp.text = getString(R.string.temperature, currentConditions.main.temp.toInt());
+        currentTemp.text = getString(R.string.temperature, currentConditions.main.temp.toInt())
     }
 }
