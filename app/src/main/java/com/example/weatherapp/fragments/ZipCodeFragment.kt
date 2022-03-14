1538eed39c1cc5050237490677d4ede3bdb1846f
Mitch Y <mitchellsimy@gmail.com>
2022-03-13 21:33:35 -0500

    Button is now accepting zip code
    The ZipCode fragment is now using the ZipCodeViewModel to accept the zip code

--- app/src/main/java/com/example/weatherapp/fragments/ZipCodeFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/ZipCodeFragment.kt
@@ -1,30 +1,65 @@
 package com.example.weatherapp.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.Layout
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.ZipcodeentryBinding
+import com.example.weatherapp.viewmodels.ZipCodeViewModel
 
 class ZipCodeFragment : Fragment(R.layout.zipcodeentry) {
 
     private lateinit var binding : ZipcodeentryBinding
+    private lateinit var viewModel: ZipCodeViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.zipcodeentry, container, false)
+        val button = view.findViewById<Button>(R.id.button)
+        val zipCode = view.findViewById<EditText>(R.id.zipCode)
+
+        binding = ZipcodeentryBinding.inflate(layoutInflater)
+        viewModel = ZipCodeViewModel()
+
+        viewModel.enableButton.observe(viewLifecycleOwner) {
+            enable -> button.isEnabled = enable
+        }
+
+        zipCode.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val newZip = p0.toString()
+                newZip.let {viewModel.updateZipCode(it)}
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+
+        })
+
 
         // When button is clicked, go here
-        val button = view.findViewById<Button>(R.id.button)
+//        view.button.setOnClickListener {
+//            findNavController().navigate(R.id.navZipToCurrentConditions)
+//        }
+
+
         button.setOnClickListener {
             findNavController().navigate(R.id.navZipToCurrentConditions)
         }
--- app/src/main/java/com/example/weatherapp/viewmodels/ZipCodeViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodels/ZipCodeViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.weatherapp.viewmodels
 
-class ZipCodeViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ZipCodeViewModel: ViewModel() {
+    private val _enableButton = MutableLiveData(false)
+
+    private var zipCode : String? = null
+    val enableButton : LiveData<Boolean>
+        get() = _enableButton
+
+    fun updateZipCode(zipCode: String) {
+        if (zipCode != this.zipCode) {
+            this.zipCode = zipCode
+            _enableButton.value = isValidZipCode(zipCode)
+        }
+    }
+
+    private fun isValidZipCode(zipCode: String): Boolean? {
+        return zipCode.length == 5 && zipCode.all {
+            it.isDigit()
+        }
+    }
+
 }
